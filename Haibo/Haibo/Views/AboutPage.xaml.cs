9ee67833024602f72302b6f7337f3138503473ef
VictorMac <vtwigge@yahoo.com>
2018-01-12 15:26:37 +0200

    New VS Project

---
+++ Haibo/Haibo/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Haibo
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
