44a1135d23206bbe7a4a2b974154681819b85da0
VictorMac <vtwigge@yahoo.com>
2018-01-15 15:36:01 +0200

    Added user views

---
+++ Haibo/Haibo/Views/userEventHistory.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Haibo.Views
+{
+    public partial class userEventHistory : ContentPage
+    {
+        public userEventHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
